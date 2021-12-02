6237977640e2cc18d78ef7350d23486e5d9cb769
rarzberger4 <raphael.arzberger@stud.fh-campuswien.ac.at>
2021-12-02 10:32:34 +0100

    added some attributes to class question for further testing

--- src/main/java/JSonInOut/JSonInOut.java
+++ src/main/java/JSonInOut/JSonInOut.java
@@ -12,18 +12,18 @@ import java.nio.file.Paths;
 
 class Question {
     String question;
-    String answer1;
-    String answer2;
-    String answer3;
-    String answer4;
+    String[] answers;
+    int difficulty;
     int answer;
 
-    public Question(String question, String answer1, String answer2, String answer3, String answer4, int answer) {
+    public Question(String question, String answer1, String answer2, String answer3, String answer4, int difficulty, int answer) {
         this.question = question;
-        this.answer1 = answer1;
-        this.answer2 = answer2;
-        this.answer3 = answer3;
-        this.answer4 = answer4;
+        this.answers = new String[4];
+        this.answers[0] = answer1;
+        this.answers[1] = answer2;
+        this.answers[2] = answer3;
+        this.answers[3] = answer4;
+        this.difficulty = difficulty;
         this.answer = answer;
     }
 
@@ -37,7 +37,7 @@ public class JSonInOut {
 
 
     public static void writetest() {
-        Question question = new Question("How long is the river danube?", "2850 km", "5550 km", "1380 km", "10700 km", 1);
+        Question question = new Question("How long is the river danube?", "2850 km", "5550 km", "1380 km", "10700 km", 2, 1);
 
         Gson gson = new Gson();
 
@@ -70,10 +70,10 @@ public class JSonInOut {
 
 
         System.out.println(question.question);
-        System.out.println(question.answer1);
-        System.out.println(question.answer2);
-        System.out.println(question.answer3);
-        System.out.println(question.answer4);
+        System.out.println(question.answers[1]);
+        System.out.println(question.answers[2]);
+        System.out.println(question.answers[3]);
+        System.out.println(question.answers[4]);
         System.out.println(question.answer);
 
 
@@ -84,7 +84,7 @@ public class JSonInOut {
 
 
         for (int i = 0; i < 10; i++) {
-            question[i] = new Question("How long is the river danube?", "2850 km", "5550 km", "1380 km", "10700 km", 1);
+            question[i] = new Question("How long is the river danube?", "2850 km", "5550 km", "1380 km", "10700 km", 2, 1);
         }
 
         Gson gson = new Gson();
@@ -117,10 +117,11 @@ public class JSonInOut {
 
         for (int i = 0; i < question.length; i++){
             System.out.println(question[i].question);
-            System.out.println(question[i].answer1);
-            System.out.println(question[i].answer2);
-            System.out.println(question[i].answer3);
-            System.out.println(question[i].answer4);
+            System.out.println(question[i].answers[0]);
+            System.out.println(question[i].answers[1]);
+            System.out.println(question[i].answers[2]);
+            System.out.println(question[i].answers[3]);
+            System.out.println(question[i].difficulty);
             System.out.println(question[i].answer);
         }
 
@@ -130,7 +131,7 @@ public class JSonInOut {
         //writetest();
         //readtest();
 
-        //writearraytest();
+        writearraytest();
         readarraytest();
 
     }
