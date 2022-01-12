de36d6cfde35e1997760a70341abab02fad96826
carme <carmen.fischer@stud-fhcampuswien.ac.at>
2022-01-12 20:12:34 +0100

    deleted if in updateHighscore

--- src/main/java/Quiz/Highscore.java
+++ src/main/java/Quiz/Highscore.java
@@ -42,35 +42,21 @@ public class Highscore {
     }
 
     //writes current known Highscores to Highscore.csv
-    //Todo: warum if in dieser Funktion?
     public void updateHighscore(String playerName, int score) throws IOException {
         String time = new SimpleDateFormat("yyyy-MM-dd_HH:mm").format(new Timestamp(System.currentTimeMillis()));
         Player p = new Player(playerName, score, time);
         highscore.add(p);
         highscore.sort((p1, p2) -> p2.getScore() - p1.getScore());
         FileWriter writer = new FileWriter("Highscore.csv");
-        if (highscore.size() < 20) {
-            for (Player player: highscore) {
-                writer.append(player.getPlayerName());
-                writer.append(",");
-                writer.append(String.valueOf(player.getScore()));
-                writer.append(",");
-                writer.append(player.getTimeStamp());
-                writer.append(System.lineSeparator());
-            }
-            writer.flush();
-            writer.close();
-        } else {
-            //Todo: umschreiben in enhanced for loop
-            for (int i = 0; i < 20; i++) {
-                writer.append(highscore.get(i).getPlayerName());
-                writer.append(",");
-                writer.append(String.valueOf(highscore.get(i).getScore()));
-                writer.append(",");
-                writer.append(highscore.get(i).getTimeStamp());
-                writer.append(System.lineSeparator());
-            }
+        for (int i = 0; i < 20|| i < highscore.size(); i++) {
+            writer.append(highscore.get(i).getPlayerName());
+            writer.append(",");
+            writer.append(String.valueOf(highscore.get(i).getScore()));
+            writer.append(",");
+            writer.append(highscore.get(i).getTimeStamp());
+            writer.append(System.lineSeparator());
         }
+
     }
 
 }
