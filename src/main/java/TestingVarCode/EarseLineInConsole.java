e51ab7a2fe18b2314a1f4e74ed1c5b285daba31d
NinaGallmetzer <91804928+NinaGallmetzer@users.noreply.github.com>
2022-01-23 13:18:51 +0100

    Create EarseLineInConsole.java

---
+++ src/main/java/TestingVarCode/EarseLineInConsole.java
@@ -0,0 +1,12 @@
+package TestingVarCode;
+
+public class EarseLineInConsole {
+
+    public static void main(String[] args) {
+        System.out.print("hello world");
+        System.out.print("\r");
+        System.out.println("good bye! ");
+
+    }
+
+}
